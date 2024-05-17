ea51a2dcce55d3a692d2d6907e953a8ee8651507
itsmebgj <bgj3088@gmail.com>
2024-05-18 02:34:41 +0900

    user_data page

---
+++ flutter/flutter_application_1/lib/user_data.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String? dropdownValue1;
+  String? dropdownValue2;
+  String? dropdownValue3;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('시간표 생성'),
+        centerTitle: true,
+        leading: TextButton(
+          onPressed: () {
+            // 로그아웃 버튼 클릭 시 처리할 로직 추가
+            print('로그아웃 버튼이 클릭되었습니다.');
+          },
+          child: Text('로그아웃'),
+        ),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: '강의년도/학기',
+                  ),
+                  value: dropdownValue1,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue1 = newValue!;
+                    });
+                  },
+                  items: <String>['2024/1', '2023/2', '2023/1']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: '학과',
+                  ),
+                  value: dropdownValue2,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue2 = newValue!;
+                    });
+                  },
+                  items: <String>['소프트웨어공학', '컴퓨터공학', '국제이공']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+            SizedBox(width: 20),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: '학년',
+                  ),
+                  value: dropdownValue3,
+                  onChanged: (String? newValue) {
+                    setState(() {
+                      dropdownValue3 = newValue!;
+                    });
+                  },
+                  items: <String>['1학년', '2학년', '3학년', '4학년']
+                      .map<DropdownMenuItem<String>>((String value) {
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // 시간표 생성 버튼 클릭 시 처리할 로직 추가
+          print('저장 버튼이 클릭되었습니다.');
+        },
+        child: Text('저장'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+    );
+  }
+}
